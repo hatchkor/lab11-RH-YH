1a4d5e900ffeda96dfffaec25d45abd83afede62
Richard Hatchko <hatchkor@ufl.edu>
2025-11-18 13:55:58 -0500

    modified calculator p2

--- calculator.py
+++ calculator.py
@@ -1,11 +1,27 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
+def add(a, b):
+    return a + b
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    try:
+        return math.log(b, a)
+    except ValueError as e:
+        print(f"{e} for a or b results in undefined output.")
+
+def exp(a, b):
+    return a**b
+
+
 
 
